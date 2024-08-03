80d9becfa5fbb8c8c311d5a4d1f0b3f9cdfb26af
Eliecer <eliecer.sanchez47@gmail.com>
2024-08-02 22:15:54 -0400

    page information added. Fetchin fixed for details

--- components/RestorantCard.jsx
+++ components/RestorantCard.jsx
@@ -34,7 +34,7 @@ export default function RestorantCard({
           <Text className="font-bold text-lg pt-2">{title}</Text>
           <View className="flex-row items-center space-x-1">
             <StarIcon color="green" opacity={0.5} size={22} />
-            <Text className="text-xs text-gray-500">
+            <Text className="text-md text-gray-500">
               <Text className="text-green-500">{rating}</Text> ● {genre}
             </Text>
           </View>

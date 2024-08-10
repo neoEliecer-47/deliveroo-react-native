4dfd90a538e6b28ebc99484aec286a98937f9e86
Eliecer <eliecer.sanchez47@gmail.com>
2024-08-09 22:01:31 -0400

    order page finished

---
+++ app/delivery.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+
+
+export default function delivery() {
+  return (
+    <View>
+      <Text>delivery</Text>
+    </View>
+  )
+}
--- app/preparingOrder.js
+++ app/preparingOrder.js
@@ -1,15 +1,36 @@
+import { useNavigation } from "expo-router";
+import { useEffect } from "react";
 import { SafeAreaView } from "react-native";
 import * as Animatable from 'react-native-animatable'
+import * as Progress from 'react-native-progress'
 
 export default function PreparingOrder() {
+
+  const navigation = useNavigation()
+
+  useEffect(()=> {
+    setTimeout(() => {
+      navigation.navigate('delivery')
+    }, 4000);
+  }, [])
+
   return (
     <SafeAreaView className='bg-[#00CCBB] flex-1 justify-center items-center'>
       <Animatable.Image 
         source={require('../assets/order loading.gif')}
         animation='slideInUp'
         iterationCount={1}
-        className='h-[25rem] w-[25rem]'
+        className='h-96 w-96'
       />
+      <Animatable.Text
+        animation='slideInUp'
+        iterationCount={1}
+        className='text-lg my-10 text-white font-bold text-center'
+      >
+        Waiting for Restaurant to accept your order!
+      </Animatable.Text>
+
+      <Progress.Circle size={60} indeterminate={true} color="white" />
     </SafeAreaView>
   );
 }

744338a2e73eb0fe5eddabf8a9b86d3364820e08
Eliecer <eliecer.sanchez47@gmail.com>
2024-08-10 00:06:49 -0400

    delivery info added

--- app/delivery.js
+++ app/delivery.js
@@ -69,6 +69,20 @@ export default function delivery() {
           pinColor="#00CCBB"
         />
       </MapView>
+
+      <SafeAreaView className='bg-white flex-row items-center space-x-5 h-28'>
+        <Image 
+            source={{
+                uri: 'https://links.papareact.com/wru'
+            }}
+            className='h-12 w-12 bg-gray-300 p-4 rounded-full ml-5'
+        />
+        <View className='flex-1 '>
+            <Text className='text-lg '>Eliecer SMora</Text>
+            <Text className='text-gray-400 '>Your Rider</Text>
+        </View>
+        <Text className='text-[#00CCBB] text-lg mr-5 font-bold'>Call</Text>
+      </SafeAreaView>
     </View>
   );
 }
